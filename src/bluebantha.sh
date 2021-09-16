434d17c81de01ef90bea158b35fa741a0cc00c6f
Dawson Mortenson <dawson.mortenson@gmail.com>
2021-09-16 00:15:43 -0700

    reduce unnecessary logging and improve connection state

--- src/bluebantha.sh
+++ src/bluebantha.sh
@@ -12,16 +12,17 @@ do
     if [ $system_state = "Yes" ]; then
         open_state=false
         cur_conn=$($blueutil --connected --format json |$jq -r '.[0].address')
-        if [ "$cur_conn" != "null" ]; then
-	    echo "[INFO] $(date) - recently closed.. disconnecting"
-            # echo $cur_conn
+        if [ ! -z "$cur_conn" ] && [ "$cur_conn" != "null" ]; then
+	        echo "[INFO] $(date) - recently closed.. disconnecting $cur_conn"
+            echo $cur_conn
             $blueutil --disconnect $cur_conn
             last_conn=$cur_conn
+            cur_conn=""
         fi
     elif ! $open_state; then
         open_state=true
         if [ "$last_conn" != "" ]; then
-            echo "[INFO] $(date) - reconnecting last device"
+            echo "[INFO] $(date) - reconnecting last device $last_conn"
             $blueutil --connect $last_conn
         fi
     else
